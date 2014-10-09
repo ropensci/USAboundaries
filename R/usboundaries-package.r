eaf3e760fde71e8d9595a563cc37fe67841b7df6
Lincoln Mullen <lincoln@lincolnmullen.com>
2014-10-09 17:53:38 -0400

    Document the package

--- R/usboundaries-package.r
+++ R/usboundaries-package.r
@@ -1,4 +1,14 @@
-#' usboundaries.
+#' usboundaries: Historical boundaries of the United States, 1629--2000
+#'
+#' This package provides spatial objects with the boundaries of states or
+#' counties in the United States of America from 1629 to 2000. It provides data
+#' from the \href{http://publications.newberry.org/ahcbp/}{Atlas of Historical
+#' County Boundaries}.
+#'
+#' The copyright in the data used in this package is owned by the Newberry
+#' Library, and it is included in this package under the terms of the Creative
+#' Commons Attribution-NonCommercial-ShareAlike 2.5 Generic (CC BY-NC-SA 2.5)
+#' license.
 #'
 #' @name usboundaries
 #' @docType package
