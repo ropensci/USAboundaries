1ac6fe853823b5cbc669f7bf5af245516bd3b453
Lincoln Mullen <lincoln@lincolnmullen.com>
2014-10-09 20:19:35 -0400

    Filter by states

--- R/us_boundaries.R
+++ R/us_boundaries.R
@@ -4,15 +4,20 @@
 #'   September 1783 and 31 December 2000; for county maps the date must be
 #'   between 30 December 1636 and 31 December 2000.
 #' @param type The type of the map, either \code{"state"} or \code{"county"}.
+#' @param states A character vector of state or territory names.
 #' @examples
 #' map_states   <- us_boundaries(as.Date("1850-07-04"))
 #' map_counties <- us_boundaries(as.Date("1850-07-04"), type = "county")
+#' new_england <- c("Massachusetts", "Vermont", "New Hampshire", "Connecticut",
+#'                  "Rhode Island", "Maine")
+#' map_ne <- us_boundaries(as.Date("1943-02-12"), states = new_england)
 #' if(require(sp)) {
 #'   plot(map_states)
 #'   plot(map_counties)
+#'   plot(map_ne)
 #' }
 #' @export
-us_boundaries <- function(map_date, type = c("state", "county")) {
+us_boundaries <- function(map_date, type = c("state", "county"), states) {
   assert_that(any(class(map_date) %in% c("POSIXct","POSIXt", "Date")))
   map_date <- as.POSIXct(map_date)
   type <- match.arg(type)
@@ -20,11 +25,23 @@ us_boundaries <- function(map_date, type = c("state", "county")) {
   if(type == "state") {
     assert_that(ymd("1783-09-03") <= map_date & map_date <= ymd("2000-12-31"))
     shp <- hist_us_states
+    states_category <- "name"
   } else {
     assert_that(ymd("1636-12-30") <= map_date & map_date <= ymd("2000-12-31"))
     shp <- hist_us_counties
+    states_category <- "state_terr"
   }
 
-  shp[shp$start_posix <= map_date & shp$end_posix >= map_date, ]
+  # Get the right dates
+  filter <- shp$start_posix <= map_date & shp$end_posix >= map_date
+
+  # Get the right states
+  if(hasArg(states)) {
+    assert_that(is.character(states))
+    filter_states <- tolower(shp[[states_category]]) %in% tolower(states)
+    filter <- filter & filter_states
+  }
+
+  shp[filter, ]
 
 }
--- tests/testthat/test-us_boundaries.R
+++ tests/testthat/test-us_boundaries.R
@@ -2,6 +2,13 @@ context("US boundaries")
 
 state  <- us_boundaries(as.Date("1865-07-04"), type = "state")
 county <- us_boundaries(as.Date("1865-07-04"), type = "county")
+newengland <- us_boundaries(as.Date("1895-04-02"), type = "state",
+                            states = c("massachusetts",
+                                       "verMONT",
+                                       "RHODE ISLAND",
+                                       "Connecticut",
+                                       "Maine",
+                                       "new Hampshire"))
 
 test_that("The correct type of object is returned", {
   expect_that(class(state), is_equivalent_to("SpatialPolygonsDataFrame"))
@@ -10,5 +17,9 @@ test_that("The correct type of object is returned", {
 
 test_that("The objects have the correct number of rows/polygons", {
   expect_that(length(state), equals(48))
-  expect_that(length(county, equals(2286))
+  expect_that(length(county), equals(2286))
+})
+
+test_that("Output can be filtered by state", {
+  expect_that(length(newengland), equals(6))
 })
