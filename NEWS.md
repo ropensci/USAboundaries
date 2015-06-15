0be4c93c9c13b9f7e0ca311f64d51b99fd4c7c28
Lincoln Mullen <lincoln@lincolnmullen.com>
2015-06-15 16:40:01 -0400

    Update documentation

--- NEWS.md
+++
@@ -1,9 +0,0 @@
-# USAboundaries 0.1.1
-
--  Fix to README.md as requested by CRAN
-
-# USAboundaries 0.1
-
--   Initial release
--   us_boundaries() returns an sp object or a data frame which can be
-    plotted.
--- R/us_boundaries.R
+++ R/us_boundaries.R
@@ -1,18 +1,20 @@
 #' Get the boundaries of the United States for a particular date
 #'
-#' This function returns
-#' @param map_date The date of the map as an object of class \code{POSIXct},
-#'   \code{POSIXt}, or \code{Date}. For state maps the date must be between 3
-#'   September 1783 and 31 December 2000; for county maps the date must be
-#'   between 30 December 1636 and 31 December 2000.
+#' This function takes a date and returns the county or state boundaries of the
+#' United States for that date.
+#'
+#' @param map_date The date of the map as some object coercable to a date with
+#'   \code{as.Date()}. For state maps the date must be between 3 September 1783
+#'   and 31 December 2000; for county maps the date must be between 30 December
+#'   1636 and 31 December 2000.
 #' @param type The type of the map, either \code{"state"} or \code{"county"}.
 #' @param states A character vector of state or territory names.
 #' @examples
-#' map_states   <- us_boundaries(as.Date("1850-07-04"))
+#' map_states   <- us_boundaries("1850-07-04")
 #' map_counties <- us_boundaries(as.Date("1850-07-04"), type = "county")
 #' new_england <- c("Massachusetts", "Vermont", "New Hampshire", "Connecticut",
 #'                  "Rhode Island", "Maine")
-#' map_ne <- us_boundaries(as.Date("1943-02-12"), states = new_england)
+#' map_ne <- us_boundaries("1943-02-12", states = new_england)
 #' if(require(sp)) {
 #'   plot(map_states)
 #'   plot(map_ne)
