1d1608e350da0349ee55c469e8bf4ae3a464c070
Hadley Wickham <h.wickham@gmail.com>
2015-06-16 13:14:30 -0500

    Update us_boundaries.R
    Some minor doc tweaks

--- R/us_boundaries.R
+++ R/us_boundaries.R
@@ -1,4 +1,4 @@
-#' Get the boundaries of the United States for a particular date
+#' Get state/county boundaries of the United States on a specified date.
 #'
 #' This function takes a date and returns the county or state boundaries of the
 #' United States for that date.
@@ -8,15 +8,18 @@
 #'   and 31 December 2000; for county maps the date must be between 30 December
 #'   1636 and 31 December 2000.
 #' @param type The type of the map, either \code{"state"} or \code{"county"}.
-#' @param states A character vector of state or territory names.
+#' @param states A character vector of state or territory names. Only boundaries 
+#'   inside these states/territories will be return.
 #' @examples
 #' map_states   <- us_boundaries("1850-07-04")
 #' map_counties <- us_boundaries(as.Date("1850-07-04"), type = "county")
 #' new_england <- c("Massachusetts", "Vermont", "New Hampshire", "Connecticut",
 #'                  "Rhode Island", "Maine")
 #' map_ne <- us_boundaries("1943-02-12", states = new_england)
-#' if(require(sp)) {
+#' 
+#' if (require(sp)) {
 #'   plot(map_states)
+#'   plot(map_counties)
 #'   plot(map_ne)
 #' }
 #' @return A \code{SpatialPolygonsDataFrame} object.
