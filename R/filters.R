3ca7f3412231159ab91a3f25f1fe2c0dca30f24b
Lincoln Mullen <lincoln@lincolnmullen.com>
2015-06-17 01:10:37 -0400

    Working version of counties function

---
+++ R/filters.R
@@ -0,0 +1,12 @@
+filter_by_date <- function(shp, map_date, start_col, end_col) {
+  filter <- as.Date(shp[[start_col]]) <= map_date &
+    as.Date(shp[[end_col]]) >= map_date
+  shp[filter, ]
+}
+
+filter_by_states <- function(shp, states, state_col) {
+  if (is.null(states)) return(shp)
+  stopifnot(is.character(states))
+  filter <- tolower(shp[[state_col]]) %in% tolower(states)
+  shp[filter, ]
+}
---
+++ R/us_counties.R
@@ -0,0 +1,18 @@
+#' @export
+us_counties <- function(map_date = NULL, resolution = c("20m"),
+                        states = NULL) {
+  resolution <- match.arg(resolution)
+  if (is.null(map_date)) {
+    shpname <- paste0("cb_2014_us_county_", resolution)
+    shp <- get(shpname)
+    shp <- filter_by_states(shp, states, "state_name")
+  } else {
+    map_date <- as.Date(map_date)
+    stopifnot(as.Date("1636-12-30") <= map_date,
+              map_date <= as.Date("2000-12-31"))
+    shp <- hist_us_counties
+    shp <- filter_by_date(shp, map_date, "start_posix", "end_posix")
+    shp <- filter_by_states(shp, states, "state_terr")
+  }
+  shp
+}
