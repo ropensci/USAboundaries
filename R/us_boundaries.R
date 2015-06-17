a1ed7bc48fb27f2751b376c7f7224b5bf13fcac1
Lincoln Mullen <lincoln@lincolnmullen.com>
2015-06-17 01:21:05 -0400

    Rewrite us_boundaries to be a wrapper

--- R/us_boundaries.R
+++ R/us_boundaries.R
@@ -8,7 +8,8 @@
 #'   and 31 December 2000; for county maps the date must be between 30 December
 #'   1636 and 31 December 2000.
 #' @param type The type of the map, either \code{"state"} or \code{"county"}.
-#' @param states A character vector of state or territory names. Only boundaries 
+#' @param resolution The resolution of the map.
+#' @param states A character vector of state or territory names. Only boundaries
 #'   inside these states/territories will be return.
 #' @examples
 #' map_states   <- us_boundaries("1850-07-04")
@@ -16,7 +17,7 @@
 #' new_england <- c("Massachusetts", "Vermont", "New Hampshire", "Connecticut",
 #'                  "Rhode Island", "Maine")
 #' map_ne <- us_boundaries("1943-02-12", states = new_england)
-#' 
+#'
 #' if (require(sp)) {
 #'   plot(map_states)
 #'   plot(map_counties)
@@ -25,34 +26,10 @@
 #' @return A \code{SpatialPolygonsDataFrame} object.
 #' @export
 us_boundaries <- function(map_date, type = c("state", "county"),
-                          states = NULL) {
-
-  map_date <- as.Date(map_date)
+                          resolution = c("20m"), states = NULL) {
   type <- match.arg(type)
-
-  if (type == "state") {
-    stopifnot(as.Date("1783-09-03") <= map_date,
-              map_date <= as.Date("2000-12-31"))
-    shp <- hist_us_states
-    states_category <- "name"
-  } else {
-    stopifnot(as.Date("1636-12-30") <= map_date,
-              map_date <= as.Date("2000-12-31"))
-    shp <- hist_us_counties
-    states_category <- "state_terr"
-  }
-
-  # Get the right dates
-  filter <- as.Date(shp$start_posix) <= map_date &
-    as.Date(shp$end_posix) >= map_date
-
-  # Get the right states
-  if (!is.null(states)) {
-    stopifnot(is.character(states))
-    filter_states <- tolower(shp[[states_category]]) %in% tolower(states)
-    filter <- filter & filter_states
-  }
-
-  shp[filter, ]
-
+  switch(type,
+    state = us_states(map_date, resolution, states),
+    county = us_counties(map_date, resolution, states)
+  )
 }
