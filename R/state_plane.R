d24e3cf2f6034e810bae5ffa8260d45ec7e362b0
Jordan Bratt <jfbratt@gmail.com>
2017-05-31 16:50:19 -0400

    Add test functions and script

--- R/state_plane.R
+++ R/state_plane.R
@@ -18,17 +18,21 @@
 #' @export
 state_plane <- function(state, plane_id = NULL, type = c("epsg", "proj4")) {
   type <- match.arg(type)
-  stopifnot(is.character(state))
-  stopifnot(is.null(plane_id) || is.character(plane_id))
+  stopifnot(state %in% unique(state_proj$state))
+  state_regions <- state_proj[state_proj$state == state, ]
+  if (!is.null(plane_id)) {
+    plane_id <- tolower(plane_id)
+  }
+  stopifnot(is.null(plane_id) || plane_id %in% unique(state_regions$zone))
   if (is.null(plane_id) && type == "epsg") {
     item <- state_proj[state_proj$state == state & state_proj$statewide_proj == TRUE, "epsg"]
-    } else if (is.null(plane_id) && type == "proj4") {
-      item <- state_proj[state_proj$state == state & state_proj$statewide_proj == TRUE, "proj4_string"]
-    } else if (type == "epsg") {
-      item <- state_proj[state_proj$state == state & toupper(state_proj$zone) == toupper(plane_id), "epsg"]
-    } else if (type == "proj4") {
-      item <- state_proj[state_proj$state == state & toupper(state_proj$zone) == toupper(plane_id), "proj4_string"]
-    }
-stopifnot(is.character(item))
-item
+  } else if (is.null(plane_id) && type == "proj4") {
+    item <- state_proj[state_proj$state == state & state_proj$statewide_proj == TRUE, "proj4_string"]
+  } else if (type == "epsg") {
+    item <- state_proj[state_proj$state == state & state_proj$zone == plane_id, "epsg"]
+  } else if (type == "proj4") {
+    item <- state_proj[state_proj$state == state & state_proj$zone == plane_id, "proj4_string"]
+  }
+  stopifnot(is.character(item))
+  item
 }
