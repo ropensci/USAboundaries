98f1d0005555ba5533895cf13dca7166350d42f5
Lincoln Mullen <lincoln@lincolnmullen.com>
2017-06-05 20:01:46 -0400

    Allow state plane to return integers for EPSG

--- R/state_plane.R
+++ R/state_plane.R
@@ -3,13 +3,16 @@
 #' Get the State Plane projection for a specific state or region within a state.
 #'
 #' @param state The state code.
-#' @param plane_id The state plane geographic zone identifier. A
-#'   \code{NULL} value will return a projection for the entire state.
-#' @param type A string identifying the output as either an EPSG code or PROJ4 string.
+#' @param plane_id The state plane geographic zone identifier. A \code{NULL}
+#'   value will return a projection for the entire state.
+#' @param type A string identifying the output as either an EPSG code or PROJ4
+#'   string.
 #'
-#' @return A string
+#' @return Either a PROJ4 string as a character vector or an EPSG code as an
+#'   integer.
 #'
-#' @seealso For documentation of the underlying state plane projection data frame, see \code{\link{state_proj}}.
+#' @seealso For documentation of the underlying state plane projection data
+#'   frame, see \code{\link{state_proj}}.
 #'
 #' @examples
 #' state_plane (state = "MA", type = "epsg")
@@ -26,13 +29,14 @@ state_plane <- function(state, plane_id = NULL, type = c("epsg", "proj4")) {
   stopifnot(is.null(plane_id) || plane_id %in% unique(state_regions$zone))
   if (is.null(plane_id) && type == "epsg") {
     item <- state_proj[["epsg"]][state_proj$state == state & state_proj$statewide_proj == TRUE]
+    item <- as.integer(item)
   } else if (is.null(plane_id) && type == "proj4") {
     item <- state_proj[["proj4_string"]][state_proj$state == state & state_proj$statewide_proj == TRUE]
   } else if (type == "epsg") {
     item <- state_proj[["epsg"]][state_proj$state == state & state_proj$zone == plane_id]
+    item <- as.integer(item)
   } else if (type == "proj4") {
     item <- state_proj[["proj4_string"]][state_proj$state == state & state_proj$zone == plane_id]
   }
-  stopifnot(is.character(item))
   item
 }
--- tests/testthat/test-state_plane.R
+++ tests/testthat/test-state_plane.R
@@ -8,14 +8,12 @@ test_that("Incorrect state value throws an error message", {
 })
 
 test_that("State plane regions can be filtered",{
-  expect_match(state_plane("MA", plane_id = "island", type = "epsg"), "26987")
-  expect_match(state_plane("MA", plane_id = "ISLAND", type = "epsg"), "26987")
+  expect_equal(state_plane("MA", plane_id = "island", type = "epsg"), 26987L)
+  expect_equal(state_plane("MA", plane_id = "ISLAND", type = "epsg"), 26987L)
   expect_identical(state_plane("FL", plane_id = "north", type = "proj4"), "+proj=lcc +lat_1=30.75 +lat_2=29.58333333333333 +lat_0=29 +lon_0=-84.5 +x_0=600000 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs")
 })
 
 test_that("Statewide projections can be filtered",{
-  expect_match(state_plane(state = "AK", type = "epsg"), "26935")
+  expect_equal(state_plane(state = "AK", type = "epsg"), 26935L)
   expect_identical(state_plane(state = "AK", type = "proj4"), "+proj=tmerc +lat_0=54 +lon_0=-154 +k=0.9999 +x_0=500000 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs")
 })
-
-
