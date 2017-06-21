e0e234575ff8c0cae0d88d042b75348b2709d07a
Lincoln Mullen <lincoln@lincolnmullen.com>
2017-06-21 17:19:23 -0400

    Add low-resolution state boundaries with documentation

---
+++ R/data-doc.R
@@ -0,0 +1,11 @@
+#' U.S. state boundaries
+#'
+#' The U.S. Census Bureau provides
+#' \href{https://www.census.gov/geo/maps-data/data/tiger-cart-boundary.html}{cartographic
+#' boundary files} for current U.S. boundaries. This packge has only the
+#' low-resolution contemporary state boundaries. Other census boundary files are
+#' provided by and documented in the USAboundariesData package.
+#' @seealso For citations for the other Census boundary files provided by the
+#'   USAboundariesData package, see this help page:
+#'   \code{\link[USAboundariesData]{census_boundaries}}.
+"states_contemporary_lores"
